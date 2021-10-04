8f18c95e4d5a1770ce4e1df516b7cae084bea19a
Romanskyy Andrey <andrey.romanskyy@icloud.com>
2021-10-04 12:59:53 +0300

    initial commit fb1. rom_func

--- ex1.py
+++ ex1.py
@@ -1,15 +1,14 @@
-
-
 def kos_greetings():
     print(f"Hello, I'm Kos!")
 
-    
+
 def def_ulka():
     print('hello')
-    
-    
+
+
 def main():
     kos_greetings()
+
 
 if __name__ == '__main__':
     print('let us start')
