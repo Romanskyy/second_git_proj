658a99cd84499309bcc3d72e0b38df7bb5887595
Romanskyy Andrey <andrey.romanskyy@icloud.com>
2021-10-04 18:05:44 +0300

    First reliase
    * initial commit fb1. rom_func
    
    * ulka commit
    
    * ready funk kos
    
    * initial commit fb1. rom_func
    
    * initial for fb2. add rom_func2
    
    * sdfsdf
    
    * reliase one
    
    Co-authored-by: UlkaBo <ul.bozhko@gmail.com>
    Co-authored-by: Константин Зивенко <kos.zivenko@gmail.com>

--- ex1.py
+++ ex1.py
@@ -4,12 +4,26 @@ def kos_greetings():
 def rom_func():
     print('Hi from Romanskyy!')
 
+def rom_func():
+    print('Hi from Romanskyy!')
+
+
+def def2_ulka():
+    pass
+
+
 def def_ulka():
     print('hello')
 
 
+# second edition
+def rom_func2():
+    print('full mess with ...ing GIT')
+
+
 def main():
     kos_greetings()
+    def_ulka()
 
 
 if __name__ == '__main__':
